b60c0f2fac9fa288e658947fd866bf95d3675b56
Jenna C <jennac0312@gmail.com>
2023-06-12 18:20:50 -0400

    progress

--- app.js
+++ app.js
@@ -118,9 +118,9 @@ for(let i = 10; i <= 400; i++){
     console.log(i)
 }
 // Write a loop that will print out every third number starting with 12 and going no higher than 4000 (including orrr???)
-for(let i = 12; i <= 4000; i+=3){
-    console.log(i)
-}
+// for(let i = 12; i <= 4000; i+=3){
+//     console.log(i)
+// }
 
 // get even
 // Print out the numbers that are within the range of 1 - 100 (means not including 100 ??)
@@ -237,3 +237,138 @@ const randomThings = [1, 10, "Hello", true]
     randomThings[2] = "World"
 // Check the value of the array to make sure it updated the array. How? Why, yes! console.log();
 console.log(randomThings)
+
+
+
+// . Change values
+// Given the following array 
+const ourClass = ["Salty", "Zoom", "Sardine", "Slack", "Github"]
+
+// What would you write to access the 3rd element of the array?
+    ourClass[2]
+// Change the value of "Github" to "Octocat"
+ourClass[ourClass.length-1] = "Octocat"
+// Add a new element, "Cloud City" to the array
+ourClass.push('Cloud City')
+
+console.log(ourClass)
+
+
+// Mix It Up
+// Note: You don't really need .splice()for these. You could use it, but there are simpler array methods that are more appropriate.
+
+// Given the following array: 
+const myArray = [5, 10, 500, 20]
+
+// Add the string "Aegon"to the end of the array. Add another string of your choice to the end of the array.
+myArray.push('Aegon')
+myArray.push("your choice")
+
+// Remove the 5from the beginning of the array.
+myArray.shift()
+
+// Add the string "Bob Marley"to the beginning of the array.
+myArray.unshift('Bob Marley')
+
+// Remove the string of your choice from the end of the array.
+myArray.pop()
+console.log('orignal array', myArray)
+// Reverse this array using Array.prototype.reverse(). Did you mutate the array? What does mutate mean? Did the .reverse()method return anything?
+
+// Array.prototype.reverse(myArray) 
+console.log(Array.prototype.reverse(myArray)) // returned Object(0) []
+console.log('prototype reverse method:', myArray) //did not mutate myArray
+
+myArray.reverse() // did mutate myArray
+console.log('reverse method:',myArray)  // returns the original array in its mutated form
+
+
+// F. Biggie Smalls
+// Create a variable that contains an integer.
+let int = 3
+
+// Write an if ... elsestatement that:
+
+// console.log()s "little number" if the number is entered is less than 100
+// console.log()s big numberif the number is greater than or equal to 100.
+
+if(int < 100){
+    console.log('little number')
+} else {
+    console.log('big number')
+}
+
+
+
+// G. Monkey in the Middle
+// Write an if ... else if ... elsestatement:
+
+// console.log()little numberif the number entered is less than 5.
+// If the number entered is more than 10, log big number.
+// Otherwise, log "monkey".
+
+if(int < 5){
+    console.log('little number')
+} else if(int > 10){
+    console.log('big number')
+} else{
+    console.log('monkey')
+}
+
+
+// H. What's in Your Closet?
+const kristynsCloset = [
+    "left shoe",
+    "cowboy boots",
+    "right sock",
+    "Per Scholas hoodie",
+    "green pants",
+    "yellow knit hat",
+    "marshmallow peeps"
+  ];
+  
+  // Thom's closet is more complicated. Check out this nested data structure!!
+  const thomsCloset = [
+    [
+      // These are Thom's shirts
+      "grey button-up",
+      "dark grey button-up",
+      "light blue button-up",
+      "blue button-up",
+    ],[
+      // These are Thom's pants
+      "grey jeans",
+      "jeans",
+      "PJs"
+    ],[
+      // Thom's accessories
+      "wool mittens",
+      "wool scarf",
+      "raybans"
+    ]
+  ];
+
+// What's Kristyn wearing today? Using bracket notation to access items in kristynsCloset, log the sentence "Kristyn is rocking that " + the third item in Kristyn's closet + " today!" to the console.
+console.log(`Kristyn is rocking that ${kristynsCloset[2]} today!`)
+
+// Kristyn just bought some sweet shades! Add "raybans"to her closet after "yellow knit hat".
+kristynsCloset.splice(6, 0, "raybans") // .splice(where(not inclusive), delete number, addWhat, addWhat ...)
+console.log(kristynsCloset)
+
+// Kristyn spilled coffee on her hat... modify this item to read "stained knit hat"instead of yellow.
+kristynsCloset[5] = "stained knit hat"
+console.log(kristynsCloset)
+// Put together an outfit for Thom! Using bracket notation, access the first element in Thom's shirtsarray.
+let shirt = thomsCloset[0][0]
+console.log(shirt)
+// In the same way, access one item from Thom's pants array.
+let pants = thomsCloset[1][0]
+// Access one item from Thom's accessories array.
+let accessories = thomsCloset[2][2]
+
+// Log a sentence about what Thom's wearing. Example: "Thom is looking fierce in a grey button-up, jeans and wool scarf!"
+
+console.log(`Thom is looking fience in a ${shirt}, ${pants}, and ${accessories}`)
+// Get more specific about what kind of PJs Thom's wearing this winter. Modify the name of his PJ pants to Footie Pajamas.
+thomsCloset[1][2] = "Footie Pajamas"
+console.log(thomsCloset)
