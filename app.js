c3e186ec2e14661b7108508b6a98db4b618e9cb1
Jenna C <jennac0312@gmail.com>
2023-06-12 16:37:44 -0400

    intial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// variables and data types
+    // How do we assign a value to a variable?
+        // assignment operator =
+        // declare with let/const/var. give value with =
+
+    // How do we change the value of a variable?
+        // re assignment with assignment operator =
+        // recall variable and assign with = (varName = value)
+
+    // How do we assign an existing variable to a new variable?
+        // with assignment operator =
+        // varName = otherVarName
+
+    // Remind me, what are declare, assign, and define?
+        // declare- let/const/var -> create space in memory with variable name
+        // assign - give value to variable with =... right side is assignment
+        // define ? - the variables have been initialized.. given a meaning... aka value.. which is just assignment
+
+    // What is pseudocoding and why should you do it?
+        // fake code written in human words
+        // plan out logic
+
+    // What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+        // however long it takes ?
+        //  90% thinking 10% coding
+
+
+
+// strings
+    // Create a variable called firstVariable
+    // Assign it the value of the string "Hello World"
+    // Change the value of this variable to some number
+    // Store the value of firstVariablein a new variable called secondVariable
+    // Change the value of secondVariableto any string.
+    // What is the value of firstVariable?
+    // Create a variable called yourNameand set it equal to your name as a string. Then, write an expression that takes the string "Hello, my name is " and the variable yourNameso that it returns a new string with them concatenated.
+
+// 
